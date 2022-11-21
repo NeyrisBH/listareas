182d7d2adb87066d093078b86ade5fe9feea27b0
NeyrisBH <113575840+NeyrisBH@users.noreply.github.com>
2022-11-20 23:07:53 -0500

    Initial commit

---
+++ src/test/java/com/miniherramientas/listareas/ListareasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miniherramientas.listareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListareasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
